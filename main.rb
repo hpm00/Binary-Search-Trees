cc97c84105ca923cbff57c2ec6b2979c11233e0a
husna <hpm25@georgetown.edu>
2022-12-16 18:42:34 -0800

    add print method

--- main.rb
+++ main.rb
@@ -37,6 +37,22 @@ class Tree
         else 
             node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
         end 
+    end 
+
+
+
+
+
+
+
+
+
+
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
-    
+
 end

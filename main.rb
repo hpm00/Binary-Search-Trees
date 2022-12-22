b13e5e2e663b47a4e539c094d6dc7b53fe926a4c
husna <hpm25@georgetown.edu>
2022-12-21 21:20:02 -0800

    add driver script

--- main.rb
+++ main.rb
@@ -156,3 +156,15 @@ class Tree
     end
 
 end
+
+# driver script (check methods)
+array = (Array.new(15) { rand(1..100) })
+tree = Tree.new(array)
+
+#Confirm that the tree is balanced by calling #balanced?
+#Print out all elements in level, pre, post, and in order
+#Unbalance the tree by adding several numbers > 100
+#Confirm that the tree is unbalanced by calling #balanced?
+#Balance the tree by calling #rebalance
+#Confirm that the tree is balanced by calling #balanced?
+#Print out all elements in level, pre, post, and in order
